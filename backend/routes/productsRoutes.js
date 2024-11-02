21e5ba8cbbfd8bd870eceeab4efe504feb4c4a2f
Ashfaq Riyaldeen <ashfaqriyaldeen@gmail.com>
2024-11-02 12:58:13 +0530

    Vercel 04

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -13,4 +13,11 @@ router.post('/', isAuth, productController.createProduct);
 router.put('/:id', isAuth, productController.updateProduct);
 router.delete('/:id', isAuth, productController.deleteProduct);
 
+module.exports = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).json({ message: 'CORS headers added' });
+  };
+
 module.exports = router;
